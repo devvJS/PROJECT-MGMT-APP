41256db4f4758bd5ddc0ffe177cdcac90ff41b1c
devvJS <d.coppage@icloud.com>
2024-10-06 01:21:42 -0400

    client query init finished

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
@@ -10,6 +11,9 @@ const app = express();
 
 //Connect to database
 connectDB();
+
+// Middleware
+app.use(cors());
 
 app.use(
   "/graphql",
